1426c1e39770b6ef698bb495ef2fd6f6e80308a2
Kevin Haag <khaag06@gmail.com>
2016-04-30 14:06:01 -0700

    add service list template

---
+++ page-services.php
@@ -0,0 +1,54 @@
+<?php require_once('header.php'); ?>
+
+    <section class="container">
+        <div class="banner">
+            <h1 class="page-head"><span>Services</span></h1>
+            <img src="img/banner-services.jpg" alt=""/>
+        </div>
+        <div class="row">
+            <div class="content extra-narrow aligncenter"  style="margin-bottom:60px;">
+                <h3 style="margin-bottom:30px;">Your Investment</h3>
+                <p>Brandy Maddison Event Planning + Design is a full-service wedding planning and event design boutique based in Kamloops and available for hire worldwide. Our services include event design, event planning, event coordination, oral design, styling, and creative direction.</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-3 aligncenter service">
+                <div class="crop portrait">
+                    <img src="img/service-1.jpg" alt="" />
+                </div>
+                <h6 class="dark">Day of Coordination</h6>
+                <a class="btn btn-gold" href="#"><span>View Details</span></a>
+            </div>
+            <div class="col-md-3 aligncenter service">
+                <div class="crop portrait">
+                    <img src="img/service-2.jpg" alt="" />
+                </div>
+                <h6 class="dark">Full Planning Service</h6>
+                <a class="btn btn-gold" href="#"><span>View Details</span></a>
+            </div>
+            <div class="col-md-3 aligncenter service">
+                <div class="crop portrait">
+                    <img src="img/service-3.jpg" alt="" />
+                </div>
+                <h6 class="dark">Design &amp; Decor</h6>
+                <a class="btn btn-gold" href="#"><span>View Details</span></a>
+            </div>
+            <div class="col-md-3 aligncenter service">
+                <div class="crop portrait">
+                    <img src="img/service-4.jpg" alt="" />
+                </div>
+                <h6 class="dark">Florals</h6>
+                <a class="btn btn-gold" href="#"><span>View Details</span></a>
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="content extra-narrow aligncenter">
+                <h3 style="margin-bottom:60px;">Let's chat about your event!</h3>
+                <p>We carefully custom-tailor each package based on your specic needs and vision, and strive to get to know each couple and their love story throughout the process. Customizeable packages are also available for styled photo shoots, corporate events, and any other special occasions inlcluding; engagements, showers, stagettes, birthdays, holidays, etc. Get in touch for more details about our services, pricing, or to book your free consultation.let’s plan and dream together!</p>
+                <a class="btn btn-gold" href="#" style="margin-top:30px;"><span>Say Hello</span></a>
+            </div>
+        </div>
+    </section>
+
+<?php require_once('footer.php'); ?>
